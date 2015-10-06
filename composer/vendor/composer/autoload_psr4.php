cfcbf1ce1c169134eab2f37d6cc5ce25eea9ae18
root <root@training3.training3>
2015-10-06 00:11:39 -0700

    Add composer php-mime-mail-parser/php-mime-mail-parser

--- composer/vendor/composer/autoload_psr4.php
+++ composer/vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PhpMimeMailParser\\' => array($vendorDir . '/php-mime-mail-parser/php-mime-mail-parser/src'),
 );
